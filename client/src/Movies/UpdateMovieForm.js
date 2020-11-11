252d9b9a4441a65b373d5b304a407998d7bb0f6c
April Darger <april.m.darger@gmail.com>
2020-11-11 13:50:24 -0700

    UpdateMovieForm. Created from, working on relevant functions.

---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const UpdateMovieForm = () => {
+
+  const initialItem = {
+    title: '',
+    director: '',
+    metascore: '',
+    
+  };
+  return (
+    <div>
+      <h2>Update Movie Info</h2>
+      <form> 
+        <input 
+          type='text'
+          name='title'
+          placeholder='Movie Title'
+        />
+        <input 
+          type='text'
+          name='director'
+          placeholder='Movie Director'
+        />
+        <input 
+          type='text'
+          name='metascore'
+          placeholder='Movie Metascore'
+        />
+      </form>
+    </div>
+  )
+
+}
